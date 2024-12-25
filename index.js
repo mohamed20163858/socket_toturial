8358b74d46f06c585531937eb133f387a54bf06c
Mohamed Saleh <mohamed20163858@gmail.com>
2024-12-25 21:09:20 +0200

    Final implementation

--- index.js
+++ index.js
@@ -4,20 +4,32 @@ const { join } = require("node:path");
 const { Server } = require("socket.io");
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
+const { availableParallelism } = require("node:os");
+const cluster = require("node:cluster");
+const { createAdapter, setupPrimary } = require("@socket.io/cluster-adapter");
+
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i,
+    });
+  }
+
+  return setupPrimary();
+}
 
 async function main() {
-  // open the database file
   const db = await open({
     filename: "chat.db",
     driver: sqlite3.Database,
   });
 
-  // create our 'messages' table (you can ignore the 'client_offset' column for now)
   await db.exec(`
     CREATE TABLE IF NOT EXISTS messages (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        client_offset TEXT UNIQUE,
-        content TEXT
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
     );
   `);
 
@@ -25,6 +37,7 @@ async function main() {
   const server = createServer(app);
   const io = new Server(server, {
     connectionStateRecovery: {},
+    adapter: createAdapter(),
   });
 
   app.get("/", (req, res) => {
@@ -42,20 +55,17 @@ async function main() {
         );
       } catch (e) {
         if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
-          // the message was already inserted, so we notify the client
           callback();
         } else {
           // nothing to do, just let the client retry
         }
         return;
       }
-      // include the offset with the message
       io.emit("chat message", msg, result.lastID);
-      // acknowledge the event
       callback();
     });
+
     if (!socket.recovered) {
-      // if the connection state recovery was not successful
       try {
         await db.each(
           "SELECT id, content FROM messages WHERE id > ?",
@@ -70,8 +80,10 @@ async function main() {
     }
   });
 
-  server.listen(3000, () => {
-    console.log("server running at http://localhost:3000");
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
   });
 }
 
